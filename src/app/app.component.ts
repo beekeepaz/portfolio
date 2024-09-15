f02280111ed5c121e5346dadd82d7982baa1f0ef
Sebastian Peters <mech.Peters@mail.de>
2024-09-16 01:10:21 +0200

    add about me section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './shared/header/header/header.component'; 
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainComponent],
+  imports: [CommonModule, RouterOutlet, 
+    MainComponent, HeaderComponent,
+  AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
