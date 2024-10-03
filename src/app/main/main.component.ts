f7cc1b9a06750f5115c202379b9545f4f5cac15b
Sebastian Peters <mech.Peters@mail.de>
2024-10-03 21:17:58 +0200

    style scrollbar

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header/header.component';
+// import { Init } from 'node:v8';
 
 @Component({
   selector: 'app-main',
@@ -9,5 +10,20 @@ import { HeaderComponent } from '../shared/header/header/header.component';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+pointimg = "./../../assets/img/point_green.png";
+
+currentIndex = 0;
+
+banneritems = ["Frontend Developer", "Based in Hannover", "Open to work", "Available to work", ];
+
+
+ngOnInit() {
+  // var elements = $('.banner').length;
+
+  // for(var i=0;i < elements; i++){
+  //  $(".tithome").clone().prependTo( ".scorri" );
+  // };
+  console.log("ngOnit Works")
+}
 
 }
