adebd9d9feedbf3a846f2f2b04e73faf6a395b9b
Sebastian Peters <mech.Peters@mail.de>
2024-10-20 23:27:43 +0200

    single-projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,8 +10,38 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   hideModal = true;
 
+  setJoin = false;
+  setElPolloLoco = false;
+  setDaBubble = false;
+
+  join: string = '';
+  elpolloloco: string = '';
+  dabubble: string = '';
+
+  backgroundClick() {
+    this.toggleModal();
+    this.setFalse();
+  }
+
+  choosenProject(ele: string) {
+    this.getSingleProject(ele);
+    this.toggleModal();
+  }
+
   toggleModal() {
     this.hideModal = !this.hideModal;
-    document.body.style.overflow = "hidden";
   }
+
+  getSingleProject(ele: string) {
+    return ele === 'Join' ? this.setJoin = true :
+      ele === 'ElPolloLoco' ? this.setElPolloLoco = true :
+        ele === 'DaBubble' ? this.setDaBubble = true : null;
+  }
+
+  setFalse() {
+    this.setJoin = false;
+    this.setElPolloLoco = false;
+    this.setDaBubble = false;
+  }
+
 }
