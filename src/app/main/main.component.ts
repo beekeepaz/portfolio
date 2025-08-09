eedcecac320740946f3929fd05102dd6f75588e2
unknown <mech.peters@mail.de>
2025-08-09 19:24:09 +0200

    responsive changes skills

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -48,7 +48,7 @@ export class MainComponent implements OnInit {
     const step = (currentTime: number) => {
       if (startTime === null) startTime = currentTime;
       const timeElapsed = currentTime - startTime;
-      const progress = Math.min(timeElapsed / duration, 1); // 0 to 1
+      const progress = Math.min(timeElapsed / duration, 1); 
       const easeInOut = progress < 0.5
         ? 2 * progress * progress
         : -1 + (4 - 2 * progress) * progress;
