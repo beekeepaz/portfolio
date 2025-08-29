d1183c3000141a9593dfa77857f7a5b2f2dd6836
unknown <mech.peters@mail.de>
2025-08-29 11:39:19 +0200

    pictures project-cart.component.ts

--- src/app/projects/project-cart/project-cart.component.ts
+++ src/app/projects/project-cart/project-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Carts } from '../../global/carts';
 import { Language } from '../../global/language';
 import { Router } from '@angular/router';
@@ -11,6 +11,31 @@ import { Router } from '@angular/router';
   styleUrl: './project-cart.component.scss'
 })
 export class ProjectCartComponent {
+  public img = {
+
+    scssCart: '../../../assets/img/scss-cart.png',
+    htmlCart: '../../../assets/img/html-cart.png',
+    angularCart: '../../../assets/img/Angular-cart.png',
+    typeScriptCart: '../../../assets/img/typeScript-cart.png',
+    javaScriptCart: '../../../assets/img/javaScript-cart.png',
+    firebaseCart: '../../../assets/img/firebase-cart.png',
+
+    joinPreviewCart: '../../assets/img/joinPreview-cart.png',
+
+    nextFirst: '../../assets/img/arrow-hirizontal.png',
+    nextSecond: '../../assets/img/arrow-horizontale-moved.png',
+
+    closeDefault: '../../../assets/img/close_small.png',
+    closeHover: '../../../assets/img/close.png',
+
+    arrowBtnDefault: '../../../assets/img/arrow_btn.png',
+    arrowBtnHover: '../../../assets/img/arrow_btn_out.png'
+  };
+
+  public isNextHovered = false;
+  public isCloseHovered = false;
+
+  public arrowBtnHover: boolean[] = [false, false];
 
   constructor(
     public carts: Carts,
@@ -18,26 +43,38 @@ export class ProjectCartComponent {
     private router: Router
   ) { }
 
-  close() {
-    this.carts.toggleModal();
-    this.carts.setFalse();
-    setTimeout(() => {
-      this.logCheckedStatus();
-    }, 100);
+  onNextEnter(): void { this.isNextHovered = true; }
+  onNextLeave(): void { this.isNextHovered = false; }
+
+  onCloseEnter(): void { this.isCloseHovered = true; }
+  onCloseLeave(): void { this.isCloseHovered = false; }
+
+  onArrowBtnEnter(idx: number): void { this.arrowBtnHover[idx] = true; }
+  onArrowBtnLeave(idx: number): void { this.arrowBtnHover[idx] = false; }
+
+  get closeIcon(): string {
+    return this.isCloseHovered ? this.img.closeHover : this.img.closeDefault;
   }
 
-  logCheckedStatus(): void {
+  arrowBtnSrc(idx: number): string {
+    return this.arrowBtnHover[idx] ? this.img.arrowBtnHover : this.img.arrowBtnDefault;
+  }
+
+  get showNextFirst(): boolean { return !this.isNextHovered; }
+  get showNextSecond(): boolean { return this.isNextHovered; }
+
+  close(): void {
+    this.carts.toggleModal();
+    this.carts.setFalse();
+    setTimeout(() => this.logCheckedStatus(), 100);
+  }
+
+  private logCheckedStatus(): void {
     const html = document.documentElement;
     html.classList.remove('no-scroll');
   }
 
-  goToPage() {
+  goToPage(): void {
     window.open('https://github.com/Simon-Kral/join', '_blank', 'noopener,noreferrer');
   }
- 
-  
-  // goToPage() {
-  //   window.location.href = 'https://github.com/Simon-Kral/join';
-  //   this.router.navigateByUrl('https://github.com/Simon-Kral/join');
-  // }
 }
