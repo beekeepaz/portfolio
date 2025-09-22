e718007560ad220e2e09053092948c391a940eec
unknown <mech.peters@mail.de>
2025-09-22 12:17:31 +0200

    style changes mobile and language

--- src/app/shared/footer/footer/footer.component.ts
+++ src/app/shared/footer/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Language } from '../../../global/language';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   constructor(
--- src/app/shared/legal-notice/legal-notice.component.ts
+++ src/app/shared/legal-notice/legal-notice.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header/header.component';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
